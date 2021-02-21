e7767863f5cad875f76ec21c00dd6a5abbd292ed
Hetansh Bhuta <hetansh.bhuta@patelhs.org>
2021-02-21 23:42:43 +0000

    added a goodbye greeting.

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ class Main { // This is the main class
     System.out.println("Enter your Number : ");
     int input = scan.nextInt(); // read the integer input 
     System.out.println("Order of Magnitude is : " + recursiveOrderOfMagnitude(input)); // print out the order of magnitude
+    System.out.println("Have a great day :)");
 }
   static int recursiveOrderOfMagnitude(int num){ // recusive method to find the order of magnitude 
     if (num <= 100) { // base case 
